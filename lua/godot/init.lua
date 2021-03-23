257d3222439fd30d6dda8919be73d50de7987086
Joakker <joaquinandresleon108@gmail.com>
2021-03-22 22:48:51 -0300

    Add logic for exporting presets

--- lua/godot/init.lua
+++ lua/godot/init.lua
@@ -1,5 +1,12 @@
 local M = {}
 
+---@alias Callback  fun(name: string, path: string)
+
+---@class Preset    @The build presets for exporting the game
+---@field name      string
+---@field path      string
+---@field callback  Callback
+
 local Job = require 'plenary.job'
 
 ---@type string
@@ -7,11 +14,15 @@ local executable
 ---@type string
 local version
 
+---@type table<string,Preset>
+M.presets = {}
+
 -- Sets up the environment for godot development
 --
----@class options:    table     The options to be passed
----@field versionmsg  boolean   Display a message at startup (default: true)
----@field executable  string    Name of the godot executable
+---@class GodotOptions
+---@field versionmsg    boolean     Display a message at startup (default: true)
+---@field executable    string      Name of the godot executable (default: godot)
+---@field presets       Preset[]    The default list of export presets
 function M.setup(options)
     executable = options.executable or 'godot'
     local versionmsg =
@@ -28,13 +39,33 @@ function M.setup(options)
         print [[Only tested with Godot 3. Tread lightly]]
     end
     if versionmsg then print('Using godot v' .. version) end
+    if options.presets then
+        for _, exp in ipairs(options.presets) do
+            M.presets[exp.name] = exp
+        end
+    end
 end
 
 -- Export the given template
 --
 ---@param name string
 function M.export(name)
-    if name then print [[Launch the export tool and export]] end
+    if name and M.presets[name] then
+        local exp = M.presets[name]
+        Job:new{
+            command = executable,
+            args = {'--export', exp.name, exp.path},
+            on_exit = function()
+                -- TODO(Joakker): Translate the on_exit callback to our
+                -- own custom handler
+            end
+        }
+    end
+end
+
+-- Export for all configured presets
+function M.export_all()
+    for name in pairs(M.presets) do M.export(name) end
 end
 
 return M
