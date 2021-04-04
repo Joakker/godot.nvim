8081c0c4d1ac73d628d453a8a8f1f85b25902efb
Joakker <joaquinandresleon108@gmail.com>
2021-04-03 21:29:13 -0300

    Find export_presets.cfg in the current and parent directories

--- lua/godot/utils.lua
+++ lua/godot/utils.lua
@@ -1,14 +1,29 @@
 local M = {}
 
+local Path = require 'plenary.path'
+
+function M.find_export_presets()
+    local path = Path:new(vim.loop.cwd())
+    if (path / 'export_presets.cfg'):exists() then
+        return 'export_presets.cfg'
+    end
+    for _, parent in ipairs(path:parents()) do
+        local testpath = Path:new(parent) / 'export_presets.cfg'
+        if testpath:exists() then testpath:absolute() end
+    end
+end
+
 function M.get_config_names()
     local names = {}
-    local conf = io.open('export_presets.cfg', 'r')
+    local filepath = M.find_export_presets()
+    if not filepath then return names end
+    local conf = io.open(filepath, 'r')
     if conf then
         for line in conf:lines() do
             if line == '' then goto continue end
-            local pair = vim.split(line, '=')
-            if pair[1] == 'name' then
-                table.insert(names, pair[2]:sub(2, #pair[2] - 1))
+            local key, value = unpack(vim.split(line, '='))
+            if key == 'name' then
+                table.insert(names, value:sub(2, #value - 1))
             end
             ::continue::
         end
